2efd699aa036fd7561aa37f650c5a2120863cb77
Andrei Florescu <48299087+AndreiFlorescu1410@users.noreply.github.com>
2019-03-11 15:50:32 +0200

    Update algorithm.h

--- algorithm.h
+++ algorithm.h
@@ -1,3 +1,6 @@
+#ifndef _algortihm_h_
+#define _algorithm_h_
+
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
@@ -5,11 +8,13 @@
 using namespace std;
 
 
-#define antet {system("CLS");cout<<"       Proiect 1 OOP -- liste circulare(alocate dinamic) -- Florescu Andrei Gr. 212         "<<endl<<endl;}
 
 class Algorithm{
     vector <int > adjacents;
 public:
     char is_adjacent(Board, int, int);
     int evaluation(Board, int, int);
+    int AlphaBeta(Board&, int, int, int, int, int, bool);
+    int NextMove(Board&);
 };
+#endif
